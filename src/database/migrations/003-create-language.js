6be4d8387b276afbf49c125f49d3a8d7c7b4019e
Ebenezer Don <ebenezerdonu@gmail.com>
2019-06-01 13:03:17 +0100

    chore(migrations): add timeStamp columns
    - add column for createdAt
    - add column for updatedAt
    
    [finishes #166394771]

--- src/database/migrations/003-create-language.js
+++ src/database/migrations/003-create-language.js
@@ -1,19 +1,28 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('Languages', {
-    id: {
-      allowNull: false,
-      primaryKey: true,
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4
-    },
-    name: {
-      allowNull: false,
-      type: Sequelize.STRING
-    },
-    version: {
-      allowNull: false,
-      type: Sequelize.STRING
-    },
-  }),
-  down: queryInterface => queryInterface.dropTable('Languages')
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Languages', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      version: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    }),
+  down: queryInterface => queryInterface.dropTable('Languages'),
 };
--- src/database/migrations/005-create-topic.js
+++ src/database/migrations/005-create-topic.js
@@ -1,18 +1,27 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('Topics', {
-    id: {
-      allowNull: false,
-      primaryKey: true,
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4
-    },
-    name: {
-      allowNull: false,
-      type: Sequelize.STRING
-    },
-    languageId: {
-      type: Sequelize.UUID
-    },
-  }),
-  down: queryInterface => queryInterface.dropTable('Topics')
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Topics', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      languageId: {
+        type: Sequelize.UUID,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    }),
+  down: queryInterface => queryInterface.dropTable('Topics'),
 };
