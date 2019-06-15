2ed4f5c1c395f4f044714e610e3abd3105f0833f
Proception <omasan.esimaje@gmail.com>
2019-06-15 13:06:51 +0100

    feature(notification): implement email notification on signup
    - add email notification
    - add verify account template
    - add email template
    - add tests
    
    [Delivers #166276523]

---
+++ src/common/notification/email/templates/emailTemplate.js
@@ -0,0 +1,70 @@
+import emailStyles from './emailStyles';
+import emailButton from './emailButton';
+import emailFooter from './emailFooter';
+
+const emailTemplate = (
+  title,
+  preheadertext,
+  user,
+  emailContent,
+  buttontext,
+  buttonLink,
+) => `
+<!doctype html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>${title}</title>
+    ${emailStyles()}
+  </head>
+  <body class="">
+    <span class="preheader">${preheadertext}</span>
+    <table role="presentation" 
+      border="0" cellpadding="0" cellspacing="0" class="body">
+      <tr>
+        <td>&nbsp;</td>
+        <td class="container">
+          <div class="content">
+
+            <!-- START CENTERED WHITE CONTAINER -->
+            <table role="presentation" class="main">
+
+              <!-- START MAIN CONTENT AREA -->
+              <tr>
+                <td class="wrapper">
+                  <table role="presentation" 
+                    border="0" cellpadding="0" cellspacing="0">
+                    <tr>
+                      <td>
+                        <p>Dear ${user || 'User'},</p>
+                        <p>${emailContent || ''}</p>
+                        ${
+                          buttontext !== undefined
+                            ? emailButton(buttontext, buttonLink)
+                            : ''
+                        }
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+
+            <!-- END MAIN CONTENT AREA -->
+            </table>
+            <!-- END CENTERED WHITE CONTAINER -->
+
+            <!-- START FOOTER -->
+              ${emailFooter()}
+            <!-- END FOOTER -->
+
+          </div>
+        </td>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+  </body>
+</html>
+`;
+
+export default emailTemplate;
