9c950f1d39b84a1dad97df65de6b851105902290
Ebenezer Don <ebenezerdon-ugwu@Connected.local>
2019-09-04 08:10:38 +0100

    chore(notification): edit notification message

--- src/common/notification/email/templates/emailTemplate.js
+++ src/common/notification/email/templates/emailTemplate.js
@@ -20,7 +20,7 @@ const emailTemplate = (
   </head>
   <body class="">
     <span class="preheader">${preheadertext}</span>
-    <table role="presentation" 
+    <table role="presentation"
       border="0" cellpadding="0" cellspacing="0" class="body">
       <tr>
         <td>&nbsp;</td>
@@ -33,11 +33,11 @@ const emailTemplate = (
               <!-- START MAIN CONTENT AREA -->
               <tr>
                 <td class="wrapper">
-                  <table role="presentation" 
+                  <table role="presentation"
                     border="0" cellpadding="0" cellspacing="0">
                     <tr>
                       <td>
-                        <p>Dear ${user || 'User'},</p>
+                        <p>Hello ${user || 'Awesome'}!</p>
                         <p>${emailContent || ''}</p>
                         ${
                           buttontext !== undefined
